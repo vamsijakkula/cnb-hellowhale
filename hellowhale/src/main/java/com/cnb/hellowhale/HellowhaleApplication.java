f0d845a1702b26fadc99e100bea5d3e2fd8456f6
vamsijakkula <krishnavamsi@gmail.com>
2020-08-21 17:15:13 +0545

    Add files via upload

---
+++ hellowhale/src/main/java/com/cnb/hellowhale/HellowhaleApplication.java
@@ -0,0 +1,13 @@
+package com.cnb.hellowhale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowhaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowhaleApplication.class, args);
+	}
+
+}
---
+++ hellowhale/src/test/java/com/cnb/hellowhale/HellowhaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnb.hellowhale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowhaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
